f8f79a02d257b55b07f229f462c332c607b8a615
blobcode <lhelme123@gmail.com>
2021-09-12 09:50:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 🌲 pine
+
+The simple rust-based reverse proxy built on top of hyper.
+
+### design
+
+the plan is for pine to take in a config file of domains and routes, decrypt traffic and pass it on to the services
---
+++ src/main.rs
@@ -0,0 +1,77 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Client, Error, Server};
+use serde_derive::Deserialize;
+use std::fs;
+use std::net::SocketAddr;
+
+// config structs
+#[derive(Debug, Deserialize)]
+struct Config {
+    port: Option<u16>,
+    #[serde(alias = "host")]
+    hosts: Vec<HostConfig>,
+}
+
+// struct for hosts in the config file
+#[derive(Debug, Deserialize)]
+struct HostConfig {
+    from: String,
+    to: String,
+}
+
+// main loop
+#[tokio::main]
+async fn main() {
+    // load config
+    let confile = fs::read_to_string("./config.toml").expect("Unable to read config file");
+    let decoded: Config = toml::from_str(&confile).unwrap();
+
+    // set server address
+    let port = decoded.port.unwrap();
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    let client_main = Client::new();
+
+    // The closure inside `make_service_fn` is run for each connection,
+    // creating a 'service' to handle requests for that specific connection.
+    let make_service = make_service_fn(move |_| {
+        // clone vars
+        let decoded: Config = toml::from_str(&confile).unwrap();
+        let client = client_main.clone();
+
+        async move {
+            // Request handler
+            Ok::<_, Error>(service_fn(move |mut req| {
+                let mut toaddr = "localhost:3000";
+                let headers = req.headers();
+                // check for host matches in the config file
+                for host in &decoded.hosts {
+                    if host.from == headers["host"] {
+                        toaddr = &host.to;
+                    }
+                }
+                // format new uri
+                let uri_string = format!(
+                    "http://{}{}",
+                    toaddr,
+                    req.uri()
+                        .path_and_query()
+                        .map(|x| x.as_str())
+                        .unwrap_or("/")
+                );
+                // request new url
+                let uri = uri_string.parse().unwrap();
+                *req.uri_mut() = uri;
+                client.request(req)
+            }))
+        }
+    });
+    // start server
+    let server = Server::bind(&addr).serve(make_service);
+    println!("server listening on http://{}", addr);
+
+    // error handling
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
