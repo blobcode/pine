4af93cf8411e2c20da133da3c0dff31a41b9a56e
Blobcode <lhelme123@gmail.com>
2021-10-14 09:48:30 -0400

    added error log class

--- src/logging.rs
+++ src/logging.rs
@@ -10,6 +10,18 @@ pub fn info(input: String) {
   log("info", Style::new(Color::Cyan), &input)
 }
 
+pub fn error(input: Option<String>) {
+  if let Some(payload) = input {
+    log("error", Style::new(Color::Red), &payload)
+  } else {
+    log(
+      "error",
+      Style::new(Color::Red),
+      "an undefined error occured",
+    )
+  }
+}
+
 // only compiles on debug builds, not in --release
 #[cfg(debug_assertions)]
 pub fn debug(input: &str) {
